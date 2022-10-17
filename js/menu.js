e8930a858eb630826055570934a48a234b1b09fd
sfirsovdev <s.firsov1985@gmail.com>
2022-10-18 02:55:47 +0300

    Закончил моб. меню

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,22 @@
-$('.menu-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-})
+(() => {
+const mobileMenu = document.querySelector('.mob-menu');
+const openMenuBtn = document.querySelector('.open-menu-btn');
+const closeMenuBtn = document.querySelector('.close-menu-btn');
+
+const toggleMenu = () => {
+    const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+};
+
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+
+})();
+
