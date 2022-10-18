abf8f7a4f6144f99cad3fe25f51d5a0bc82543e8
sfirsovdev <s.firsov1985@gmail.com>
2022-10-19 00:43:34 +0300

    Готово бургер меню

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,5 @@
 document.querySelector('.burger').addEventListener('click',
     function () {
         document.querySelector('.burger span').classList.toggle('active');
+        document.querySelector('.mob-menu-top ').classList.toggle('animate');
     }) 
