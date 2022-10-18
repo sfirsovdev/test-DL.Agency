3fb305fee09039c149704ec9fab70ec4cc2b31ae
sfirsovdev <s.firsov1985@gmail.com>
2022-10-18 21:27:25 +0300

    Добавил структуру главной

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+type="text/javascript">
+    $(document).ready(function () {
+            $('#pagepiling').pagepiling( {
+                sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'red'],
+            });
+        });
