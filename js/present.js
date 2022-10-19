351c751de26723ff53208722544abcc17ca7b1bd
sfirsovdev <s.firsov1985@gmail.com>
2022-10-20 00:41:50 +0300

    Блок about
    Заканчиваю

---
+++ js/present.js
@@ -0,0 +1,7 @@
+$('[data-open-block').on('click', function() {
+	const activeCls = 'is-active';
+
+	$('[data-content]').removeClass(activeCls);
+    $(`[data-content="${$(this).data('open-block')}"`).addClass(activeCls);
+});
+
