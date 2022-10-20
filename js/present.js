a5ed5b98339dcd48423204b7d99a6d8bb001c384
sfirsovdev <s.firsov1985@gmail.com>
2022-10-20 14:39:11 +0300

    Настройка блока about

--- js/present.js
+++ js/present.js
@@ -2,6 +2,9 @@ $('[data-open-block').on('click', function() {
 	const activeCls = 'is-active';
 
 	$('[data-content]').removeClass(activeCls);
-    $(`[data-content="${$(this).data('open-block')}"`).addClass(activeCls);
+	$(`[data-content="${$(this).data('open-block')}"`).addClass(activeCls);
+	$('[data-open-block').removeClass(activeCls);
+	$(this).addClass(activeCls);
 });
+
 
