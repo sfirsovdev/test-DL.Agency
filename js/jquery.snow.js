08e2cdc7655a14b6f59cbafa47536ee55580395e
sfirsovdev <s.firsov1985@gmail.com>
2022-10-22 10:30:32 +0300

    Поправил vw

--- js/jquery.snow.js
+++ js/jquery.snow.js
@@ -1,21 +1,3 @@
-/**
- * jquery.snow - jQuery Snow Effect Plugin
- *
- * Available under MIT licence
- *
- * @version 1 (21. Jan 2012)
- * @author Ivan Lazarevic
- * @requires jQuery
- * @see http://workshop.rs
- *
- * @params flakeChar - the HTML char to animate
- * @params minSize - min size of snowflake, 10 by default
- * @params maxSize - max size of snowflake, 20 by default
- * @params newOn - frequency in ms of appearing of new snowflake, 500 by default
- * @params flakeColors - array of colors , #FFFFFF by default
- * @params durationMillis - stop effect after duration
- * @example $.fn.snow({ maxSize: 200, newOn: 1000 });
- */
 (function($){
 	
 	$.fn.snow = function(options){
@@ -37,7 +19,7 @@
 
 			var interval		= setInterval( function(){
 				var startPositionLeft 	= Math.random() * documentWidth - 100,
-				 	startOpacity		= 0.5 + Math.random(),
+				    startOpacity		= 0.5 + Math.random(),
 					sizeFlake			= options.minSize + Math.random() * options.maxSize,
 					endPositionTop		= documentHeight - defaults.maxSize - 40,
 					endPositionLeft		= startPositionLeft - 100 + Math.random() * 200,
--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,8 @@
-type="text/javascript">
-    $(document).ready(function () {
+$(document).ready(function () { 
+if (matchMedia('only screen and (min-width: 480px)').matches) {
+    
             $('#pagepiling').pagepiling( {
                 sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'red'],
             });
-        });
+}
+    });
