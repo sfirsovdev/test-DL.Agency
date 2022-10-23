2f554fb72973ec5f4e61ffb23166792d84550a5b
sfirsovdev <s.firsov1985@gmail.com>
2022-10-23 11:38:00 +0300

    Скрол фикс

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,6 @@
 document.querySelector('.burger').addEventListener('click',
     function () {
         document.querySelector('.burger span').classList.toggle('active');
+        document.body.classList.toggle('overflow-hidden');
         document.querySelector('.mob-menu-top ').classList.toggle('animate');
     }) 
